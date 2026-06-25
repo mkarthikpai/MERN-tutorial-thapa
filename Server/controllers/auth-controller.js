dae796c726662b1bef4524cc5cc816c336a8b24a
M Karthik Pai <mkarthikpai25@gmail.com>
2026-06-25 15:51:12 +0530

    Created user schema and inserted to DB with POSTMAN

--- Server/controllers/auth-controller.js
+++ Server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("Hello Auth from Controller!");
@@ -8,8 +10,14 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).send("Welcome to Registration!");
+    const { username, email, phone, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+
+    const userCreated = await User.create({ username, email, phone, password });
+    res.status(200).json({ message: userCreated });
   } catch (error) {
     res.status(400).send({ msg: error });
   }
