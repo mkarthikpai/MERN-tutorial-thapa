8f36e9b2878205eee8e8ed655a13d1e406d2a224
M Karthik Pai <mkarthikpai25@gmail.com>
2026-07-01 18:42:25 +0530

    Navbar Development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Contact from "./pages/Contact";
 import Service from "./pages/Service";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
