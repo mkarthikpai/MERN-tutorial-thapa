cf93050daf3896f0802fa85551de39885d6ee451
M Karthik Pai <mkarthikpai25@gmail.com>
2026-07-04 23:26:11 +0530

    Login form development

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,78 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/Register.png"
+                  alt="registration"
+                  width={"400"}
+                  height={"500"}
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Login Form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={user.email}
+                      onChange={handleInput}
+                      placeholder="Enter Email"
+                      required
+                      autoComplete="off"
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={user.password}
+                      onChange={handleInput}
+                      placeholder="Enter Password"
+                      required
+                      autoComplete="off"
+                    />
+                  </div>
+                  <br />
+                  <button className="btn btn-submit" type="submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 };
 
 export default Login;
