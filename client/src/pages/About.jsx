5d6801b28683ae52205c579246539fb52588380c
M Karthik Pai <mkarthikpai25@gmail.com>
2026-08-03 23:26:43 +0530

    Added a context value to the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,13 +1,19 @@
 import { NavLink } from "react-router-dom";
 import Analytics from "../components/Analytics";
+import { useAuth } from "../store/auth";
 
 const About = () => {
+  const { user } = useAuth();
+
   return (
     <>
       <section className="section-hero">
         <div className="container grid grid-two-cols">
           <div className="hero-content">
-            <p>Welcome, Karthik</p>
+            <p>
+              Welcome,{" "}
+              {user ? `${user.username} to our website` : "to our website"}
+            </p>
             <h1>Why choose us ?</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
